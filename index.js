286a6f9b0abc01df6118e13a3ea8b58f2ea70cc6
kagankas <reagankasulekagan@gmail.com>
2024-11-01 14:11:18 +0300

    # Portfoil

---
+++ index.js
@@ -0,0 +1,8 @@
+//console.log("Hello World");
+//const body = document.getElementsByTagName('body');
+//body.style.backgroundColor="red";
+//document.body.style.backgroundColor = "lightblue";
+//console.log("trna");
+//alert("greene");
+console.log("here ");
+document.getElementsByTagName("P").style.Color="red";
